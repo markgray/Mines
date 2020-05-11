e276b8e708586a498420b163a1bf67b6d56c4861
markgray <mark.t.gray@gmail.com>
2020-05-11 05:40:57 -0400

    Another baby step towards an actual game

--- app/src/main/java/com/example/android/mines/MainActivity.kt
+++ app/src/main/java/com/example/android/mines/MainActivity.kt
@@ -6,17 +6,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import androidx.gridlayout.widget.GridLayout
 
 @SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var board : GridLayout
-    private lateinit var good : Button
-    private lateinit var mine : Button
-    private var boardWidth : Int = 0
-    private var boardHeight : Int = 0
+    private lateinit var board: GridLayout
+    private lateinit var good: Button
+    private lateinit var mine: Button
+    private var screenWidth: Int = 0
+    private var screenHeight: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,25 +25,35 @@ class MainActivity : AppCompatActivity() {
         board = findViewById(R.id.board_grid)
         good = findViewById(R.id.button_good)
         mine = findViewById(R.id.button_mine)
-        boardWidth = resources.displayMetrics.widthPixels
-        boardHeight = resources.displayMetrics.heightPixels - 200
+        screenWidth = resources.displayMetrics.widthPixels
+        screenHeight = resources.displayMetrics.heightPixels - 200
         createBoard(this)
     }
 
+    @Suppress("UNUSED_PARAMETER")
     private fun createBoard(context: Context): View {
 
         board.columnCount = COLUMN_COUNT
         board.rowCount = ROW_COUNT
 
-        val cellWidth = boardWidth / COLUMN_COUNT
-        val cellHeight = boardHeight / ROW_COUNT
+        val cellWidth = screenWidth / COLUMN_COUNT
+        val cellHeight = screenHeight / ROW_COUNT
+        val cellSize = if (cellWidth < cellHeight) {
+            cellWidth
+        } else {
+            cellHeight
+        }
 
-        for (column in 1..COLUMN_COUNT) {
-            for (row in 1..ROW_COUNT) {
+        for (row in 1..ROW_COUNT) {
+            for (column in 1..COLUMN_COUNT) {
                 val textView = TextView(this)
-                textView.width = cellWidth
-                textView.height = cellHeight
-                textView.text = "$column,$row"
+                textView.width = cellSize
+                textView.height = cellSize
+                textView.setPadding(4, 4, 4, 4)
+                textView.text = "($row,$column)"
+                textView.setOnClickListener {
+                    it.setBackgroundColor(Color.GRAY)
+                }
                 board.addView(textView)
             }
         }
