c1317fa812b46ebb1136b0955bc8adab8e98bac5
markgray <mark.t.gray@gmail.com>
2020-06-06 05:23:54 -0400

    Removed unnecessary SharedViewModel code from MainActivity

--- app/src/main/java/com/example/android/mines/MainActivity.kt
+++ app/src/main/java/com/example/android/mines/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.android.mines
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("MemberVisibilityCanBePrivate", "RemoveExplicitTypeArguments")
 class MainActivity : AppCompatActivity() {
 
-    lateinit var viewModel: SharedViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val model: SharedViewModel by viewModels<SharedViewModel>()
-        viewModel = model
         setContentView(R.layout.activity_main)
     }
 
