76ed5830dab9f28415a88bc53142c19e80ff3c7c
markgray <mark.t.gray@gmail.com>
2020-06-14 06:27:50 -0400

    Edited comments

--- app/src/main/java/com/example/android/mines/database/MinesDatum.kt
+++ app/src/main/java/com/example/android/mines/database/MinesDatum.kt
@@ -22,25 +22,46 @@ data class MinesDatum
     @PrimaryKey(autoGenerate = true)
     var gameId: Long = 0L,
 
+    /**
+     * The number of columns our game board uses. The name of the column in the database is
+     * "number_of_columns"
+     */
     @ColumnInfo(name = "number_of_columns")
     val numColumns: Int = -1,
 
+    /**
+     * The number of rows our game board uses. The name of the column in the database is
+     * "number_of_rows"
+     */
     @ColumnInfo(name = "number_of_rows")
     val numRows: Int = -1,
 
+    /**
+     * The number of Mines our game board has. The name of the column in the database is
+     * "number_of_mines"
+     */
     @ColumnInfo(name = "number_of_mines")
     val numMines: Int = 0,
 
+    /**
+     * The current time when the [MinesDatum] was constructed, which is the difference, measured in
+     * milliseconds, between the current time and midnight, January 1, 1970 UTC. The name of the
+     * column in the database is "game_date_and_time".
+     */
     @ColumnInfo(name = "game_date_and_time")
     val startTimeMilli: Long = System.currentTimeMillis(),
 
+    /**
+     * The number of milliseconds that it took to play the game. The name of the column in the
+     * database is "game_elapsed_time".
+     */
     @ColumnInfo(name = "game_elapsed_time")
     var elapsedTimeMilli: Long = 0,
 
     /**
      * Each character in this [String] field represents a sector in the SharedViewModel.haveMines
      * list. Sectors with Mines are encoded as a "*" character, safe sectors are encoded as a blank
-     * character. The name of the column in the database is "sectors_with_mines"
+     * character. The name of the column in the database is "sectors_with_mines".
      */
     @ColumnInfo(name = "sectors_with_mines")
     var haveMines: String
