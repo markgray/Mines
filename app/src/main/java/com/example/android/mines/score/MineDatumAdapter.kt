db6a52bdb72080b3195d748ea98ff34a2ee2bc20
markgray <mark.t.gray@gmail.com>
2025-02-01 06:49:41 -0500

    Updated kotlin to 2.1.10 and updated gradle dependencies

--- app/src/main/java/com/example/android/mines/score/MineDatumAdapter.kt
+++ app/src/main/java/com/example/android/mines/score/MineDatumAdapter.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.mines.R
 import com.example.android.mines.SharedViewModel
 import com.example.android.mines.database.MinesDatum
 import com.example.android.mines.databinding.MineDatumViewBinding
