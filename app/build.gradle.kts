f20fadbc5ebe20d269f6452e70774cb1503991df
markgray <mark.t.gray@gmail.com>
2025-03-12 06:59:54 -0400

    Updated AGP to 8.9.0 and updated gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@
 plugins {
     id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "2.1.10" apply false
-    id("com.google.devtools.ksp") version "2.1.10-1.0.30" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.31" apply false
 }
